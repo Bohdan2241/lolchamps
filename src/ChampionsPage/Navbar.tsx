ac4b0f240011739e3ad03d2fe5c818256a4d3c4e
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-04-27 00:21:55 +0200

    init Navbar

---
+++ src/ChampionsPage/Navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <section>
+      <nav>
+        <div>
+          <div>
+            <div>Search</div>
+            <div>
+              <ul className="hidden">Desktop tabs</ul>
+              <div>All roles</div>
+            </div>
+            <div>all difficulties</div>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+}
+
+export default Navbar;
