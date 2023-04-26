112a0c3b6b5da45a941af68750251eba9c236c73
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-04-26 02:08:15 +0200

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '1/2': '50%',
+      }
+    },
+    fontFamily: {
+      custom: ["Beaufort for LOL", 'serif'],
+    },
   },
   plugins: [],
 };
