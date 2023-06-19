191e323e0e1eebeedba0f52c4489a72a2ebc548f
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-06-20 01:35:24 +0200

    update paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Header from './Components/Header';
-import AllChampions from './Pages/AllChampions';
 import Champion from './Pages/Champion';
-import NotFound from './Pages/NotFound';
+import ChampionList from './Pages/ChampionList';
+import NotFound from './Pages/NotFound/NotFound';
 import routes from './routes';
 
 const App = () => (
   <Router>
     <Header />
     <Routes>
-      <Route path={routes.championsListPagePath()} element={<AllChampions />} />
+      <Route path={routes.championsListPagePath()} element={<ChampionList />} />
       <Route path={routes.championPagePath()} element={<Champion />} />
       <Route path={routes.notFoundPagePath()} element={<NotFound />} />
     </Routes>
