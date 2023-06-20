6a5a82788b7db93f923be4f70ffa47183b3b1072
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-06-21 01:07:49 +0200

    add i18next

---
+++ src/locales/en.ts
@@ -0,0 +1,27 @@
+export default {
+  translation: {
+    'champion-link.start': 'champions/',
+    'champion-list.filter-result.no-champions-found':
+      'No champions match the filter criteria.',
+    'champion-difficulty.all-difficulties': 'All Difficulties',
+    'search.message.no-champions-found': 'No champions found.',
+    'search.action': 'Search',
+    'champion-difficulty.ranking.low': 'Low',
+    'champion-difficulty.ranking.medium': 'Moderate',
+    'champion-difficulty.ranking.high': 'High',
+    'champion-role.all.short': 'All',
+    'champion-role.assassin.plural': 'Assassins',
+    'champion-role.fighter.plural': 'Fighters',
+    'champion-role.mage.plural': 'Mages',
+    'champion-role.marksman.plural': 'Marksmen',
+    'champion-role.support.plural': 'Supports',
+    'champion-role.tank.plural': 'Tanks',
+    'section.champion-list.title': 'choose your\nchampion',
+    'section.champion-list.description':
+      'With more than 140 champions, you’ll find the perfect match for your playstyle. Master one, or master them all.',
+    loading: 'Loading...',
+    error: 'Oh no, there was an error',
+    'error.404': 'Error 404',
+    pageNotFound: 'The page you were looking for was not found.',
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import { GlobalStyle } from './assets/styles/style';
 import { store } from './store';
 
-import './assets/styles/index.css';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
