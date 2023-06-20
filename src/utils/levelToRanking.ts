15b1f8a7036638761688729729a110f6eb459128
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-06-21 01:11:12 +0200

    update util

--- src/utils/levelToRanking.ts
+++ src/utils/levelToRanking.ts
@@ -1,5 +1,13 @@
 import ChampionDifficultyRanking from '../enums/championDifficultyRanking';
 
+export const CHAMPION_DIFFICULTIES: {
+  [x in ChampionDifficultyRanking]: string;
+} = {
+  [ChampionDifficultyRanking.LOW]: 'champion-difficulty.ranking.low',
+  [ChampionDifficultyRanking.MEDIUM]: 'champion-difficulty.ranking.medium',
+  [ChampionDifficultyRanking.HIGH]: 'champion-difficulty.ranking.high',
+};
+
 export default (num: number): ChampionDifficultyRanking => {
   if (num >= 8) return ChampionDifficultyRanking.HIGH;
   if (num >= 4) return ChampionDifficultyRanking.MEDIUM;
