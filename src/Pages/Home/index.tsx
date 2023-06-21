da08c1c8de8d49854b2d5eb4275552e275fe1689
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-06-21 23:35:10 +0200

    add Home page

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import homeLogo from '../../assets/homeLogo.png';
+
+const Home = () => {
+  return (
+    <div>
+      <img src={homeLogo} alt="logo" />
+    </div>
+  );
+};
+
+export default Home;
