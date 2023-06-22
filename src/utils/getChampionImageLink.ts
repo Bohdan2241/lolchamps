7175b207370bdfb237d43ee06190f94068afd0c2
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-06-22 02:06:21 +0200

    fix small size option

--- src/utils/getChampionImageLink.ts
+++ src/utils/getChampionImageLink.ts
@@ -1,4 +1,8 @@
 const getChampionImageLink = (championId: string, size: string) => {
+  if (size === 'small') {
+    return `https://ddragon.leagueoflegends.com/cdn/13.12.1/img/champion/${championId}.png`;
+  }
+
   const baseURL = 'http://ddragon.leagueoflegends.com/cdn/img/champion/';
   return `${baseURL}${size}/${championId}_0.jpg`;
 };
